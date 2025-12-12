90f35b1b4ae9e4c865069110b1c7664e75ab60be
unknown <bieniaszkacper03@gmail.com>
2025-12-12 12:17:47 +0100

    feat: upload passenger model

--- pkg/simulation/passenger/passenger_store.go
+++ pkg/simulation/passenger/passenger_store.go
@@ -1,6 +1,9 @@
 package passenger
 
 import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
 	"math/rand/v2"
 
 	"github.com/TNSEngineerEdition/WailsClient/pkg/city"
@@ -25,8 +28,6 @@ func NewPassengersStore(c *city.City) *PassengersStore {
 		}
 	}
 
-	store.generatePassengers(c)
-
 	return store
 }
 
@@ -34,7 +35,7 @@ func (ps *PassengersStore) GetPassengerCountAtStop(stopID uint64) uint {
 	return ps.PassengersAtStops[stopID].GetPassengerCount()
 }
 
-func (ps *PassengersStore) generatePassengers(c *city.City) {
+func (ps *PassengersStore) GeneratePassengers(c *city.City) {
 	timeBounds := c.GetTimeBounds()
 	tramStops := c.GetStops()
 	var counter uint64
@@ -62,6 +63,63 @@ func (ps *PassengersStore) generatePassengers(c *city.City) {
 			ps.PassengersToSpawn[spawn] = append(ps.PassengersToSpawn[spawn], passenger)
 			counter++
 		}
+	}
+}
+
+func (ps *PassengersStore) GeneratePassengersDueModel(c *city.City, passengerModel []byte) error {
+	reader := csv.NewReader(bytes.NewReader(passengerModel))
+	reader.Comma = ','
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		return fmt.Errorf("error reading passenger model csv: %w", err)
+	}
+
+	if len(records) == 0 {
+		return fmt.Errorf("passenger model csv is empty")
+	}
+
+	header := records[0]
+	if len(header) < 4 {
+		return fmt.Errorf("invalid header, expected at least 4 columns, got %d", len(header))
+	}
+
+	for i, row := range records[1:] {
+		lineNo := i + 2
+
+		if len(row) < 4 {
+			return fmt.Errorf("line %d: expected 4 columns, got %d", lineNo, len(row))
+		}
+
+		// startName := strings.TrimSpace(row[0])
+		// endName := strings.TrimSpace(row[1])
+		// spawnTimeStr := strings.TrimSpace(row[2])
+		// strategyStr := strings.TrimSpace(row[3])
+
+		// t, err := time.Parse("15:04:05", spawnTimeStr)
+		// if err != nil {
+		//     return fmt.Errorf("line %d: invalid spawn_time %q: %w", lineNo, spawnTimeStr, err)
+		// }
+
+		// spawnSeconds := t.Hour()*3600 + t.Minute()*60 + t.Second()
+
+		// spawn := PassengerSpawn{
+		//     StartStopName: startName,
+		//     EndStopName:   endName,
+		//     SpawnSeconds:  spawnSeconds,
+		//     Strategy:      SpawnStrategy(strategyStr),
+		// }
+
+		// spawns = append(spawns, spawn)
+	}
+
+	fmt.Printf("git")
+	return nil
+}
+
+func (ps *PassengersStore) ResetPassengers() {
+	for _, stop := range ps.PassengersAtStops {
+		stop.passengers = stop.passengers[:0]
 	}
 }
 
